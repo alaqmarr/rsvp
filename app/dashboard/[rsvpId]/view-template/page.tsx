f0eb366e85c64731c5f4177013f8db7efed64635
Al Aqmar <alaqmarak0810@gmail.com>
2025-01-20 13:05:10 +0530

    template change

--- app/dashboard/[rsvpId]/view-template/page.tsx
+++ app/dashboard/[rsvpId]/view-template/page.tsx
@@ -1,8 +1,9 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/db";
+import { CalendarCheck, ClockIcon, LocateFixedIcon, StarsIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -30,20 +31,15 @@ const Template = async ({
       <Card className="bg-gradient-to-r from-amber-200 to-yellow-400 max-w-[80vw] min-w-[300px] shadow-md gap-y-3 rounded-lg">
         <CardHeader>
           <CardTitle className="flex items-center justify-center text-center w-full">
-            <p>{data.name}</p>
-          </CardTitle>
-        </CardHeader>
-        <Separator className=" mb-3" />
-        <CardContent className="flex flex-col items-center justify-center w-full gap-y-3">
           <div className="flex items-center justify-center w-full text-center">
             <p className="font-[kanz] font-bold text-3xl">
               بسم الله الرحمان الرحيم
             </p>
           </div>
-
-          <div className="flex flex-col items-start justify-center w-full">
-            <p className="font-semibold text-xl underline">To: John Doe</p>
-          </div>
+          </CardTitle>
+        </CardHeader>
+        <Separator className="bg-black mb-3" />
+        <CardContent className="flex flex-col items-center justify-center w-full gap-y-3">
 
           <div className="flex flex-col items-end justify-center w-full">
             <p className="font-[marjaan] font-semibold text-xl text-end">
@@ -55,16 +51,39 @@ const Template = async ({
             <p className="font-bold text-wrap break-all">{data.description}</p>
           </div>
 
-          <div className="flex flex-col items-start justify-center w-full uppercase">
-            <p>
-              Venue: <strong>{data.venue}</strong>
-            </p>
-            <p>
-              Date: <strong>{new Date(data.date).toDateString()}</strong>
-            </p>
-            <p>
-              Time: <strong>{data.time}</strong>
-            </p>
+          <div className="flex flex-col items-start justify-center w-full uppercase gap-y-2">
+            <div className="flex items-center justify-start w-full">
+              <span className="mr-3">
+              <StarsIcon/>
+              </span>
+              <p>
+                <strong>{data.name}</strong>
+              </p>
+            </div>
+            <div className="flex items-center justify-start w-full">
+              <span className="mr-3">
+              <LocateFixedIcon/>
+              </span>
+              <p>
+                <strong>{data.venue}</strong>
+              </p>
+            </div>
+            <div className="flex items-center justify-start w-full">
+              <span className="mr-3">
+              <CalendarCheck/>
+              </span>
+              <p>
+                <strong>{new Date(data.date).toDateString()}</strong>
+              </p>
+            </div>
+            <div className="flex items-center justify-start w-full">
+              <span className="mr-3">
+              <ClockIcon/>
+              </span>
+              <p>
+                <strong>{data.time}</strong>
+              </p>
+            </div>
           </div>
 
           <Separator />
@@ -76,11 +95,9 @@ const Template = async ({
             </p>
           </div>
         </CardContent>
-      </Card>
-
-      <Separator className="bg-black" />
-
-      <Alert
+        <Separator className="bg-black mb-3" />
+        <CardFooter className="flex flex-col items-center justify-center w-full">
+        <Alert
         variant={"destructive"}
         className="w-fit flex flex-col gap-y-3 bg-red-300"
       >
@@ -93,7 +110,9 @@ const Template = async ({
           </Button>
         </AlertDescription>
       </Alert>
-    </div>
+        </CardFooter>
+      </Card>
+      </div>
   );
 };
 
